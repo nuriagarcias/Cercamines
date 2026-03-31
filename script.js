5ede9d1e1ba746d50e13c25049f5dce8ce35360b
nuriagarcias <nuriagarcias@alumnat.iesdamiahuguet.net>
2026-03-31 11:05:25 +0200

    casi acabat

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 const mida = 10;
 let matriu = [];
 let mines = 15;
+let resposta=[]
+let numBanderes = 15
 
 // Arrancada inicial
-inicialitzarMatriu();
+inicialitzarMatriu1();
 renderitzar();
 
 
-function inicialitzarMatriu() {
+function inicialitzarMatriu1() {
+
     for (let i = 0; i < mida; i++) {
         matriu[i] = [];
+        resposta[i]=[];
         for (let j = 0; j < mida; j++) {
             matriu[i][j] = 0; // 0 = Negre / Apagat
+            resposta[i][j] = '';
         }
     }
 }
 
 //Funció que calcularà on estan ubicades les mines
 function colocarMines(){
-    inicialitzarMatriu();
+    inicialitzarMatriu1();
+    numBanderes = 14;
+    document.getElementById("boto").innerHTML = "Reiniciar"
+    document.getElementById("banderes").innerHTML = "🚩: 15"
+    document.getElementById("missatges").innerHTML = "Hi ha 15 mines"
     for(let i=0; i < mines; i++) {
         let x=Math.trunc(mida*Math.random());
         let y=Math.trunc(mida*Math.random());
         console.log(x,y);
         if(matriu[x][y] === 0){
             matriu[x][y] = 1;
+            resposta[x][y] = '*';
         } else{
             i--
         }
@@ -34,10 +44,31 @@ function colocarMines(){
     renderitzar();
 }
 
+function MostrarRespostes() {
+    const container = document.getElementById('grid-container');
+    container.innerHTML = ""; // Esborrem l'anterior
+
+    for (let i = 0; i < mida; i++) {
+        for (let j = 0; j < mida; j++) {
+            const div = document.createElement('div');
+            div.classList.add('cell');
+
+            if (resposta[i][j] === '*') {
+                div.classList.add('bomba');
+            } else {
+                if (resposta[i][j] !== '') {
+                    div.classList.add('active')
+                    div.innerText = resposta[i][j];
+                }
+            }
+            container.appendChild(div);
+        }
+    }
+}
 
 
 // function netejar() {
-//     inicialitzarMatriu();
+//     inicialitzarMatriu1();
 //
 //     renderitzar();
 // }
@@ -53,50 +84,85 @@ function renderitzar() {
             div.classList.add('cell');
 
             // Si a la matriu hi ha un 1, afegim la classe 'active' (color verd)
-            if (matriu[i][j] === 2) {
-                div.classList.add('active');
-            }
-            else if (matriu[i][j] === 1) {
-                div.classList.add('bomba');
-            }
+            // if (matriu[i][j] === 2) {
+            //     div.classList.add('active');
+            // }
+            // else if (matriu[i][j] === 1) {
+            //     div.classList.add('bombaAmagada');
+            // }
             div.addEventListener('click', function () {
 
                 if (matriu[i][j] === 1) {
-                    alert('boom')
-                    inicialitzarMatriu();
+                    div.classList.add('bomba')
+                    document.getElementById("missatges").innerHTML = "Has perdut!";
+                    MostrarRespostes();
                 } else {
-                    calcular()
-                    console.log(comptador);
+                    div.classList.add('active')
+                    if (resposta[i][j] === '') {
+                        p=document.createElement("p")
+                        let numero= calcular(i,j)
+                        resposta[i][j] = numero;
+                        p.textContent= numero;
+                        if(numero === 1){
+                            p.style.color = "#0053a6"
+                        }
+                        div.appendChild(p);
+                    }
+                    //calcular(i,j)
+
+                    // console.log(calcular(i,j));
                 }
-                renderitzar();
+                //renderitzar();
+            })
+            div.addEventListener('contextmenu', function () {
+                event.preventDefault();
+
+               if (div.classList.contains('bandera')) {
+                   div.classList.remove('bandera');
+                   div.innerText = "";
+                   numBanderes++
+               }
+               else{
+                   div.classList.add('bandera');
+                   div.innerText= "🚩"
+                   numBanderes--
+               }
+               document.getElementById("banderes").innerHTML = "🚩: " + numBanderes; //renderitzar();
             })
 
             container.appendChild(div);
         }
     }
 
-    }
+}
 
-    function calcular(){
-        let comptador=0
-        if(matriu[i-1][j-1]){
-            comptador++
-        }
-        if(matriu[i+1][j+1]){
-            comptador++;
-        }
-        if(matriu[i][j-1]){
-            comptador++;
-        }
-        if(matriu[i][j+1]){
-            comptador++;
-        }
-        if(matriu[i-1][j]){
-            comptador++;
-        }
-        if(matriu[i+1][j]){
-            comptador++;
-        }
-
+function calcular(i, j) {
+    let comptador = 0
+    if (matriu[i - 1]?.[j - 1] === 1) {
+        comptador++
     }
+    if (matriu[i + 1]?.[j + 1] === 1) {
+        comptador++;
+    }
+    if (matriu[i]?.[j - 1] === 1) {
+        comptador++;
+    }
+    if (matriu[i]?.[j + 1] === 1) {
+        comptador++;
+    }
+    if (matriu[i - 1]?.[j] === 1) {
+        comptador++;
+    }
+    if (matriu[i + 1]?.[j] === 1) {
+        comptador++;
+    }
+    if (matriu[i - 1]?.[j + 1] === 1) {
+        comptador++;
+    }
+    if (matriu[i + 1]?.[j - 1] === 1) {
+        comptador++;
+    }
+    return comptador
+
+}
 
