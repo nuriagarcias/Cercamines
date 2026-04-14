bf9a0510d42d85529c2a85f62e4ac322774afefd
nuriagarcias <nuriagarcias@alumnat.iesdamiahuguet.net>
2026-04-14 11:05:04 +0200

    falta llevar zeros i poder guanyar

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let matriu = [];
 let mines = 15;
 let resposta=[]
 let numBanderes = 15
+let banderesPolsades= 0
+let cellsPolsats= 0
 
 // Arrancada inicial
 inicialitzarMatriu1();
@@ -24,7 +26,7 @@ function inicialitzarMatriu1() {
 //Funció que calcularà on estan ubicades les mines
 function colocarMines(){
     inicialitzarMatriu1();
-    numBanderes = 14;
+    numBanderes = 15;
     document.getElementById("boto").innerHTML = "Reiniciar"
     document.getElementById("banderes").innerHTML = "🚩: 15"
     document.getElementById("missatges").innerHTML = "Hi ha 15 mines"
@@ -58,7 +60,18 @@ function MostrarRespostes() {
             } else {
                 if (resposta[i][j] !== '') {
                     div.classList.add('active')
+
+                    if (resposta[i][j] === 1) div.style.color = "#007fff"
+                    if (resposta[i][j] === 2) div.style.color = "#008a41"
+                    if (resposta[i][j] === 3) div.style.color = "#d50030"
+                    if (resposta[i][j] === 4) div.style.color = "#0e008a"
+                    if (resposta[i][j] === 5) div.style.color = "#7600b1"
+                    if (resposta[i][j] === 6) div.style.color = "#00a598"
+                    if (resposta[i][j] === 7) div.style.color = "#000000"
+                    if (resposta[i][j] === 8) div.style.color = "#737373"
+                    if (resposta[i][j] === 0) div.style.color = "#ceebff"
                     div.innerText = resposta[i][j];
+
                 }
             }
             container.appendChild(div);
@@ -98,16 +111,35 @@ function renderitzar() {
                     MostrarRespostes();
                 } else {
                     div.classList.add('active')
-                    if (resposta[i][j] === '') {
-                        p=document.createElement("p")
-                        let numero= calcular(i,j)
-                        resposta[i][j] = numero;
-                        p.textContent= numero;
-                        if(numero === 1){
-                            p.style.color = "#0053a6"
+                    if(!div.classList.contains('bandera')){
+
+                        if (resposta[i][j] === '') {
+                            cellsPolsats++;
+                            console.log(cellsPolsats);
+                            p=document.createElement("p")
+                            let numero= calcular(i,j)
+                            resposta[i][j] = numero;
+                            p.textContent= numero;
+
+                            if(numero === 1) p.style.color = "#007fff"
+                            if (numero === 2) p.style.color = "#008a41"
+                            if (numero === 3) p.style.color = "#d50030"
+                            if (numero === 4) p.style.color = "#0e008a"
+                            if (numero === 5) p.style.color = "#7600b1"
+                            if (numero === 6) p.style.color = "#00a598"
+                            if (numero === 7) p.style.color = "#000000"
+                            if (numero === 8) p.style.color = "#737373"
+                            if (numero === 0) p.style.color = "#ceebff"
+
+                            div.appendChild(p);
+
                         }
-                        div.appendChild(p);
                     }
+                    if (cellsPolsats+banderesPolsades === mida*mida){
+                        document.getElementById("missatges").innerHTML = "Has guanyat!";
+                        MostrarRespostes();
+                    }
+
                     //calcular(i,j)
 
                     // console.log(calcular(i,j));
@@ -121,13 +153,15 @@ function renderitzar() {
                    div.classList.remove('bandera');
                    div.innerText = "";
                    numBanderes++
+                   banderesPolsades--
                }
-               else{
+               else if (!div.classList.contains('active')) {
                    div.classList.add('bandera');
                    div.innerText= "🚩"
                    numBanderes--
+                   banderesPolsades++
                }
-               document.getElementById("banderes").innerHTML = "🚩: " + numBanderes; //renderitzar();
+               document.getElementById("banderes").innerHTML = "🚩: " + numBanderes;
             })
 
             container.appendChild(div);
